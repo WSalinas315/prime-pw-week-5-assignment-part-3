4d0eda230d8a05dd13af7895c91e2858625585a4
Billy Salinas <wsalinas315@gmail.com>
2022-10-21 17:07:44 -0500

    Created collection array and addToCollection function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,15 @@
 console.log('***** Music Collection *****')
+
+// Define collection array
+let collection = [];
+
+// Function to add objects with title, artist and year published to the existing collection array
+function addToCollection(title, artist, yearPublished){
+    let album = {
+        title: title,
+        artist: artist,
+        year: yearPublished
+    }
+    collection.push(album);
+    return true;
+}
